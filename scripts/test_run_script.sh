faa42f1311410085f9b1f7e12064c90ddaba4e68
itchy <richard.h.styron@gmail.com>
2014-04-22 22:49:58 -0400

    got (locally) running test_run_script.sh and made other small changes

--- scripts/test_run_script.sh
+++ scripts/test_run_script.sh
@@ -2,32 +2,32 @@
 
 cd ~eqsarah
 
-job_id = 45LM3
+job_id=TEST1
+pecube_dir=pecube_temps/$job_id/Pecube
 
-pecube_dir = pecube_temps/$job_id/Pecube
-
-
-
+mkdir -p ssrd_pecube/comparisons
 
 # make temp directory for running Pecube, copy relevant stuff
-mkdir pecube_temps/$job_id
+mkdir -p pecube_temps/$job_id
 
-mkdir $pecube_dir/data
-mkdir $pecube_dir/input
-mkdir $pecube_dir/bin
-mkdir $pecube_dir/$job_id
+mkdir -p $pecube_dir/data
+mkdir -p $pecube_dir/input
+mkdir -p $pecube_dir/bin
+mkdir -p $pecube_dir/$job_id
 
-cp src/bin/Pecube $pecube_dir/bin
-cp src/bin/Pecube/data/*.* $pecube_dir/data/*.*
-cp src/bin/Pecube/input/*.* $pecube_dir/input/*.*
+cp -R src/Pecube/bin $pecube_dir
+cp -R src/Pecube/data $pecube_dir
+cp -R src/Pecube/input $pecube_dir
 
 # run Pecube
 cd $pecube_dir
-ipython test_run_pecube.py $job_id
+#ipython test_run_pecube.py $job_id
+bin/Pecube
 
+cp $job_id/Comparison.txt ~eqsarah/ssrd_pecube/comparisons/Comparison.$job_id.txt
 
 # clean up
-cd ~/eqarah
+cd ~eqsarah
 rm -rf $pecube_dir
 
 exit 0
